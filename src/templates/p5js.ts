cb0cef0f15def1c2c5c097bead3e7a3ace47afd2
scarscarin <scarscarin@gmail.com>
2025-07-27 19:25:14 +0200

    changed p5 template

--- src/templates/p5js.ts
+++ src/templates/p5js.ts
@@ -1,23 +1,36 @@
 export const P5_INDEX_HTML = `<!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <title>p5.js sketch</title>
-    <link rel="stylesheet" href="style.css" />
-    <script src="https://cdn.jsdelivr.net/npm/p5@1.9.0/lib/p5.min.js"></script>
-    <script src="sketch.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/p5@latest/lib/p5.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+
   </head>
   <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
   </body>
-</html>`;
+</html>
+`;
 
-export const P5_STYLE_CSS = `body { margin: 0; padding: 0; }
-canvas { display: block; }`;
+export const P5_STYLE_CSS = `html, body {
+  margin: 0;
+  padding: 0;
+}
+canvas {
+  display: block;
+}
+`;
 
 export const P5_SKETCH_JS = `function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  background("palevioletred");
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
 }`;
